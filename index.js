d6db8896bdc1528c4de4e90538abb563cd2649b4
Natália Ribeiro <62111939+Natalia0702@users.noreply.github.com>
2020-07-27 13:55:53 -0300

    projetoAPI - primeiro commit
    primeiro commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//import axios from 'axios';
+//const accessToken = '617f09431faac7ecaa51ee5941bd43a1';
+//const apiUrl = 'api.atrialub.com.br/exemplo';
+
+//var axios = require('react-axios');
+//--Formas de conexão com API----------//
+
+//axios.defaults.baseURL = 'api.atrialub.com.br/exemplo';
+//axios.defaults.headers.common['Authorization'] = 617f09431faac7ecaa51ee5941bd43a1;
+//axios.defaults.headers.post['Content-Type'] =
+//'application/x-www-form-urlencoded';
+//(erro )Uncaught or
+
+//--Formas de conexão com API----------//
+
+//axios.interception.request
+//.use
+//(config => ( {
+//coonfig.headers.authorization = `Baerer {$accessToken}`;
+//return config;
+//}
+//(error) => {
+//return promisse.reject(error);
+//};
+window.addEventListener('load', function () {});
